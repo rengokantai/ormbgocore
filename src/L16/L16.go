dfc12e151869c3bc610fda02680612744fbb51a4
Hernan Y.Ke <yuriqiao@gmail.com>
2015-09-18 23:58:39 -0400

    16

---
+++ src/L16/L16.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+"fmt"
+)
+
+func emit(wordChannel chan string, done chan bool){
+
+words := []string{"The","quick","brown","fox"}
+i:=0
+
+for {
+select{
+case wordChannel <-words[i]:
+i+=1
+if i==len(words){
+i=0
+}
+case<-done:
+close(done)
+return
+}
+
+}
+
+}
+
+
+func main(){
+
+
+wch := make(chan string)
+doch:= make(chan bool)
+
+for i:=0,i <100; i++{
+fmt.Print("%s ", <-wch)
+}
+
+doch <-true
+
+
+}
